9a2e52decbf9db180d8054587a7fa138153ed341
LucCESI <luc.melaye@gmail.com>
2024-12-06 00:56:08 +0100

    feat : nom randomisé

--- BACK_Nuit2Info/Controllers/PokeApiController.cs
+++ BACK_Nuit2Info/Controllers/PokeApiController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace BACK_Nuit2Info.Controllers
 {
@@ -11,9 +14,32 @@ namespace BACK_Nuit2Info.Controllers
     {
         private readonly PokeApiClient pokeClient;
 
+        private static List<string> PokemonNames = new List<string>();
+
         public PokeApiController()
         {
             pokeClient = new PokeApiClient();
+        }
+        private static async Task LoadPokemonNamesAsync()
+        {
+            if (PokemonNames.Count == 0)
+            {
+                string apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=200";
+
+                using HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var jsonDocument = JsonDocument.Parse(jsonResponse);
+                var results = jsonDocument.RootElement.GetProperty("results");
+
+                foreach (var result in results.EnumerateArray())
+                {
+                    string name = result.GetProperty("name").GetString();
+                    PokemonNames.Add(name);
+                }
+            }
         }
 
         [HttpGet(Name = "GetPokemon")]
@@ -21,19 +47,24 @@ namespace BACK_Nuit2Info.Controllers
         {
             try
             {
-                string name = "ho-oh";
-                Pokemon poke = await pokeClient.GetResourceAsync<Pokemon>(name);
+                await LoadPokemonNamesAsync();
+
+                Random random = new Random();
+                int randomIndex = random.Next(PokemonNames.Count);
+                string randomName = PokemonNames[randomIndex];
+
+                Pokemon poke = await pokeClient.GetResourceAsync<Pokemon>(randomName);
 
                 if (poke == null || poke.Sprites == null)
                 {
                     throw new Exception("Les données du Pokémon sont nulles ou incomplètes.");
                 }
-
                 var jsonObj = new
                 {
-                    nom = name,
+                    nom = randomName,
                     img = poke.Sprites.FrontDefault
                 };
+
                 return JsonSerializer.Serialize(jsonObj);
             }
             catch (Exception ex)
